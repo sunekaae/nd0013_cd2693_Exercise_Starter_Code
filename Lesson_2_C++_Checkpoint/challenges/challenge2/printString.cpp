cd399d619c23b51e0482fad5a8a0b1768277b592
Sune Kaae <sunekaae@gmail.com>
2025-11-23 12:09:46 +0100

    C++ lesson 2 challenge 2 done

--- Lesson_2_C++_Checkpoint/challenges/challenge2/printString.cpp
+++ Lesson_2_C++_Checkpoint/challenges/challenge2/printString.cpp
@@ -5,4 +5,7 @@
 void printString (std::string str, int n) {
     // your code goes here! print str n times. Follow each str with a newline
     // eg. std::cout << str << std::endl;
+    for (int i = 0; i<n; i++) {
+        std::cout << str << "\n";
+    }
 }
