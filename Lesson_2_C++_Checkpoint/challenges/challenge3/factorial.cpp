a3dfd7b0d0b1ac4ea345529c5b2717f410618bd1
Sune Kaae <sunekaae@gmail.com>
2025-11-23 12:30:03 +0100

    C++ lesson 2 challenge 3 done

--- Lesson_2_C++_Checkpoint/challenges/challenge3/factorial.cpp
+++ Lesson_2_C++_Checkpoint/challenges/challenge3/factorial.cpp
@@ -3,3 +3,12 @@
 // Your code goes here
 // See factorial.h for your method's signature 
 // (eg. its args and return value)
+int factorial(int n) {
+    if (n==1) {
+        return 1;
+    }
+    else {
+        int factorial_result = factorial(n-1);
+        return n*factorial_result;
+    }
+}
