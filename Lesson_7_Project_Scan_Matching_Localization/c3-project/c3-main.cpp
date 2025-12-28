0bb3db5e09bcaa121c633cf453091a47a5941523
Sune Kaae <sunekaae@gmail.com>
2025-12-28 09:26:26 +0100

    first ICP attempt. with declared transformed_scan

--- Lesson_7_Project_Scan_Matching_Localization/c3-project/c3-main.cpp
+++ Lesson_7_Project_Scan_Matching_Localization/c3-project/c3-main.cpp
@@ -181,6 +181,8 @@ int main(){
 	Point poseRefPclPos(-t0.location.y, t0.location.x, -t0.location.z);
 	double maxError = 0;
 
+	PointCloudT::Ptr transformed_scan (new PointCloudT);
+
 	while (!viewer->wasStopped())
   	{
 		while(new_scan){
@@ -226,8 +228,12 @@ int main(){
 
 			// TODO: Find pose transform by using ICP or NDT matching
 			//pose = ....
+			Eigen::Matrix4d transform = transform3D(pose.rotation.yaw, pose.rotation.pitch, pose.rotation.roll, pose.position.x, pose.position.y, pose.position.z);
+			transform = ICP(mapCloud, cloudFiltered, pose, 3);
+			pose = getPose(transform);
 
 			// TODO: Transform scan so it aligns with ego's actual pose and render that scan
+			pcl::transformPointCloud (*cloudFiltered, *transformed_scan, transform);
 
 			viewer->removePointCloud("scan");
 			// TODO: Change `scanCloud` below to your transformed scan
